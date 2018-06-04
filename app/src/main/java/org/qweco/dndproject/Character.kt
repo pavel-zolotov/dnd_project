cb72bb071208a227dcab2011145090e43cc8b4e0
enger <pvl.zolotov@gmail.com>
2018-06-04 18:45:32 +0300

    Character class improvements

--- app/src/main/java/org/qweco/dndproject/Character.kt
+++ app/src/main/java/org/qweco/dndproject/Character.kt
@@ -12,12 +12,16 @@ data class Character (
         var armourClass: Int,
         var proficiency: Int,
 
-        var skills: Array<Int>,
-        var savingThrows: Array<Int>,
+        var skills: Array<Int>, //use constants from class body, pls
+        var savingThrows: Array<Int>, //here the same
 
         var strenght: Int,
         var dexterity: Int,
         var constution: Int,
         var intelligence: Int,
         var wisdom: Int,
-        var charisma: Int)
+        var charisma: Int){
+    //Skills
+    val ACROBATICS = 1
+    val ANIMAL_HANDLING = 2
+}
