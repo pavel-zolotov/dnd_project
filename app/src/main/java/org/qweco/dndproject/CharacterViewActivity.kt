8c4b5b8cde63b5906acca67fdfce4a7cd1366fe6
trrttrtrsrfewrwed <timurruskiy2@mail.ru>
2018-06-06 13:07:27 +0300

    new activity WITH XML ALLALALAl

---
+++ app/src/main/java/org/qweco/dndproject/CharacterViewActivity.kt
@@ -0,0 +1,12 @@
+package org.qweco.dndproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_view)
+    }
+}
